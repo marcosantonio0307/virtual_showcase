f23d0b62eed73121dbc495a8b94ea95a25980033
Marcos Antonio <marcosantoniotaina@gmail.com>
2020-02-11 00:14:48 -0300

    setting purge method and bucket from images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_one_attached :photo, dependent: :destroy
+  has_one_attached :photo, dependent: :purge
 
   def thumbnail
   	return self.photo.variant(resize: '300x300')
